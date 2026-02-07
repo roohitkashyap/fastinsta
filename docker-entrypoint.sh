83bf3ebca0b37bcf818a95332bfe8bc0bfd9cd41
Rohit Kashyap <roohitkashyap@gmail.com>
2026-02-07 18:10:34 +0530

    Fix Apache MPM error in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,11 @@ set -e
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT:-80}/g" /etc/apache2/sites-enabled/000-default.conf
 
+# Force disable conflicting MPMs and enable prefork (required for PHP)
+a2dismod -f mpm_event || true
+a2dismod -f mpm_worker || true
+a2enmod mpm_prefork || true
+
 # Run migrations (force)
 php artisan migrate --force
 
